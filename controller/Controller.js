2de13124342990077642bfdb90424007009cec52
vietlinhg4 <vietlinhg7@gmail.com>
2024-01-04 22:26:17 +0700

    complete login page

--- controller/Controller.js
+++ controller/Controller.js
@@ -2,6 +2,62 @@ const controller = {};
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
+controller.showLogin = (req, res) => {
+    let reqUrl = req.query.reqUrl ? req.query.reqUrl : '/';
+    if (req.session.user) {
+        return res.redirect(reqUrl);
+    }
+    res.render('login', {
+        layout: 'login-signup',
+        reqUrl,
+        username: req.signedCookies.username,
+        password: req.signedCookies.password,
+    });
+};
+
+controller.login = async (req, res) => {
+    let { account, password, rememberMe } = req.body;
+    console.log(account);
+    let user = await User.findOne({ userName: account, password });
+    if (user) {
+        let reqUrl = req.body.reqUrl ? req.body.reqUrl : '/';
+        req.session.user = user;
+        if (rememberMe) {
+            res.cookie('username', username, {
+                maxAge: 60 * 60 * 1000,
+                httpOnly: false,
+                signed: true,
+            });
+            res.cookie('password', password, {
+                maxAge: 60 * 60 * 1000,
+                httpOnly: true,
+                signed: true,
+            });
+        }
+        return res.redirect(reqUrl);
+    }
+    return res.render('login', {
+        layout: 'login-signup',
+        message: 'Sai tên tài khoản hoặc mật khẩu',
+    });
+};
+
+controller.logout = (req, res, next) => {
+    req.session.destroy(function (error) {
+        if (error) return next(error);
+        res.redirect('/login');
+    });
+};
+
+controller.isLoggedIn = async (req, res, next) => {
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+        return next();
+    }
+    else {
+        res.redirect('/login');
+    }
+};
 
 controller.showIndex = async (req, res) => {
 

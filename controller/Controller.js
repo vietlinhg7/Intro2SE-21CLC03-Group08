8c06e6ea11019019dbe8a5d9d043222b6e68e57e
quocti2003 <lejack526@gmail.com>
2024-01-05 12:32:59 +0700

    updated logo

--- controller/Controller.js
+++ controller/Controller.js
@@ -94,7 +94,7 @@ controller.isLoggedIn = async (req, res, next) => {
 controller.showIndex = async (req, res) => {
     let product = await Product.find({});
     let in_cartproduct = await Product.find({ in_cart: 1 });
-    res.render('product_details', {
+    res.render('index', {
         layout: 'index',
         product: product,
         in_cartproduct: in_cartproduct,
